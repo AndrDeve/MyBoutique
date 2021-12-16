3cbbe9d13f420071898fc882172e95b8a7cd21b1
AndyR <marlon_rivera95@hotmail.com>
2021-12-16 14:48:46 -0500

    se agrega el proyecto

---
+++ js/scroll.js
@@ -0,0 +1,35 @@
+ScrollReveal().reveal('.container__nav', { delay: 200 });
+ScrollReveal().reveal('.titulo__logo', { delay: 200 });
+ScrollReveal().reveal('.titulo__opiniones', { delay: 200 });
+ScrollReveal().reveal('.titulo__mapa', { delay: 200 });
+ScrollReveal().reveal('.redes__footer', { delay: 200 });
+
+
+window.sr = ScrollReveal();
+
+    sr.reveal('.scroll1' , {
+        duration: 2000,
+        origin: 'bottom',
+        distance: '10px'
+    });
+
+window.sr = ScrollReveal();
+
+    sr.reveal('.scroll2' , {
+        duration: 3000,
+        origin: 'bottom',
+        distance: '10px'
+    });  
+    
+window.sr = ScrollReveal();
+
+    sr.reveal('.scroll3' , {
+        duration: 4000,
+        origin: 'bottom',
+        distance: '10px'
+    }); 
+
+
+
+    
+
